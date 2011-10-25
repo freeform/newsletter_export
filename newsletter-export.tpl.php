0e75c1e0b3de092ad5b0c63f44d15e8dbb0f5f4d
Arvin Singla <arvin.singla@freeformsolutions.ca>
2011-10-25 10:36:27 -0400

    Initial Commit

---
+++ newsletter-export.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file newsletter_export.tpl.php
+ * Template to output a node as html for newsletters
+ *
+ * - $raw_markup: The basic html markup for all fields you would get from the content portion of a node.
+ * - $fields: An array of all fields belonging to the node including body 
+ */
+?>
+
+<?php print $raw_markup ?>
