060d581fb1e97f8e0c62929d9b75daf06fd595a0
Lynna Landstreet <lynna@freeform.ca>
2014-02-06 12:31:14 -0500

    Merging in last few changes made to the version of the module on Drupal.org (fix for relative links issue by Lynna and some previous changes by Lola).

--- newsletter-export.tpl.php
+++ newsletter-export.tpl.php
@@ -9,6 +9,11 @@
  * - $raw_markup: The basic html markup for all fields you would get from the content portion of a node.
  * - $fields: An array of all fields belonging to the node including body
  * - $node: The entire node object
+ * 
+ * Note that if you want to use $fields or $node in your template, you may need to add
+ * some code to fix relative links, since currently newsletter_export_fix_links only
+ * works on $raw_markup.
+ * 
  */
 ?>
 
